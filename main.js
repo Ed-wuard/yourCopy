79f7a0ea7d32d3b84ed14af0f6e313a33882f8cc
Edmadriz <eduarddaniel128@gmail.com>
2024-04-11 19:50:24 -0400

    refactor: :recycle: Se movio el codigo js a un archivo externo

---
+++ main.js
@@ -0,0 +1,119 @@
+const textArea = document.getElementById('textArea');
+const copyBtn = document.getElementById('copyBtn');
+const list = document.getElementById('list');
+const helpBtn = document.getElementById('helpBtn');
+
+window.addEventListener('load', () => {
+    const hasSeenModal = localStorage.getItem('hasSeenModal');
+
+    if (hasSeenModal !== 'true') {
+        const howToUseModal = new bootstrap.Modal(document.getElementById('howToUseModal'));
+        howToUseModal.show();
+
+        const howToUseModalElement = document.getElementById('howToUseModal');
+        howToUseModalElement.addEventListener('hidden.bs.modal', () => {
+            localStorage.setItem('hasSeenModal', 'true');
+        });
+    }
+});
+
+textArea.addEventListener('contextmenu', async (e) => {
+    e.preventDefault(); // Evitar que se muestre el menú contextual predeterminado
+
+    try {
+        const clipboardContent = await navigator.clipboard.readText();
+        const startPosition = textArea.selectionStart;
+        const endPosition = textArea.selectionEnd;
+
+        textArea.value = textArea.value.substring(0, startPosition) + clipboardContent + textArea.value.substring(endPosition);
+
+        // Establecer la nueva posición del cursor después de pegar el contenido
+        textArea.selectionStart = startPosition + clipboardContent.length;
+        textArea.selectionEnd = startPosition + clipboardContent.length;
+    } catch (error) {
+        console.error('Error al acceder al portapapeles:', error);
+    }
+});
+
+
+function showToast() {
+    const toast = new bootstrap.Toast(document.getElementById('copyToast'));
+    toast.show();
+}
+
+function loadData() {
+    const storedData = JSON.parse(localStorage.getItem('listData')) || [];
+    storedData.forEach((text) => {
+        const li = createListItem(text);
+        list.appendChild(li);
+    });
+}
+
+function saveData() {
+    const listItems = Array.from(list.children).map((li) => li.textContent.trim().replace(/delete/i, ''));
+    localStorage.setItem('listData', JSON.stringify(listItems));
+}
+
+copyBtn.addEventListener('click', () => {
+    const text = textArea.value;
+    if (text.trim()) {
+        navigator.clipboard.writeText(text);
+        showToast();
+    } else {
+        alert('Por favor, ingresa un texto para copiar');
+    }
+});
+
+function createListItem(text) {
+    const li = document.createElement('li');
+    li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center');
+
+    const span = document.createElement('span');
+    span.textContent = text;
+    li.appendChild(span);
+
+    // Agregar evento de clic para copiar el texto del elemento
+    li.addEventListener('click', (e) => {
+        if (e.target !== deleteBtn) { // Verificar si el clic fue en el botón "delete"
+            navigator.clipboard.writeText(text);
+            showToast();
+        }
+    });
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('btn', 'btn-danger', 'ms-2');
+    deleteBtn.innerHTML = '<i class="material-icons">delete</i>';
+    deleteBtn.addEventListener('click', (e) => {
+        e.stopPropagation(); // Detener la propagación del evento de clic
+        li.remove();
+        saveData(); // Guardar los datos en el localStorage
+    });
+
+    li.appendChild(deleteBtn);
+    return li;
+}
+
+
+function addItemToList() {
+    const text = textArea.value;
+    if (text.trim()) {
+        const li = createListItem(text);
+        list.appendChild(li);
+        textArea.value = '';
+        saveData(); // Guardar los datos en el localStorage
+    }
+}
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+        e.preventDefault();
+        addItemToList();
+    }
+});
+
+helpBtn.addEventListener('click', () => {
+    const howToUseModal = new bootstrap.Modal(document.getElementById('howToUseModal'));
+    howToUseModal.show();
+});
+
+loadData(); // Cargar los datos almacenados en el localStorage
